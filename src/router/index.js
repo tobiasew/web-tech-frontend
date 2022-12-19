8f81be93e8a60378ac157c904d9dc12f80562e5b
tobiasewald <tobias.ewald@student.htw-berlin.de>
2022-12-19 22:28:56 +0100

    add github action for CI

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Home from '../views/HomeView'
+import About from '../views/AboutView'
+import Liste from '../views/Liste'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/liste',
+    name: 'Liste',
+    component: Liste
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
   routes
 })
 
---
+++ src/views/Liste.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Welcome to Liste</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Liste'
+}
+</script>
+
+<style scoped>
+
+</style>
